3edc1d07f3cbc779fb425d64858e5ad92fac6bf8
AXDNJUPT <3112772832@qq.com>
2025-09-17 10:45:31 +0800

    Site updated: 2025-09-17 10:45:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/08/hello-world/","2025/07/09/ssh/","2025/07/08/怎么用hexo创建blog/"];function toRandomPost(){
+var posts=["2025/09/15/B站前钰/","2025/07/08/hello-world/","2025/07/30/jupyter设置与启动/","2025/07/09/ssh/","2025/07/30/pytorch语法/","2025/09/17/wang/","2025/09/09/创建一个纯净的虚拟环境/","2025/07/08/怎么用hexo创建blog/","2025/09/09/有能的AI/","2025/09/10/神经网络常识/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
